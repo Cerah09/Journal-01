6cfb4b932ce1b5a1c2f5a467f95f8bac26043b55
Cerah09 <jpanjaitan082@gmail.com>
2023-09-27 09:41:08 +0700

    Jurnal-01

---
+++ Jurnal.java
@@ -0,0 +1,7 @@
+public class Jurnal {
+    public static void main(String[] args) {
+        System.out.println("NAMA :Josua NATANAEL PANJAITAN");
+        System.out.println("NIM : 607062330056");
+        System.out.println("Kelas : D3IF-47-04");
+    }
+}
